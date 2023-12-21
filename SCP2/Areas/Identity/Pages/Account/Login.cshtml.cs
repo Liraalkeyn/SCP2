867b0c7ba652b1acc87a8e8dcc0611e19901506a
Liraalkeyn <mikunar2018@gmail.com>
2023-12-21 15:32:22 +0300

    Refactoring

--- SCP2/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SCP2/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,6 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
+#nullable disable //Тут бэк напрямую интегрирован в фронт, и это КОШМАР
 
 using System;
 using System.Collections.Generic;
